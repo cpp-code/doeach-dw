2b0f88b0449e181036d15171ebcbc0efbe6ade1a
doeach-dw <doeach@qq.com>
2019-12-27 10:57:49 +0800

    Regular expression
    Resolve #1
    
    Signed-off-by: doeach-dw <doeach@qq.com>

--- Re_expression.cc
+++ Re_expression.cc
@@ -1,21 +1,21 @@
-#include<iostream>
-#include<regex>
+#include <iostream>
+#include <regex>
 using namespace std;
 
-int main(){
-    string str = "aaBBcc123";
-    regex pat("([a-zA-Z]{6})(\\d{3})");
-    smatch result;
-    bool a = regex_match(str, result, pat);
-    if(a){
-        cout << "ture：" << result[1] << endl;
-        cout << result.size();
-        for(int i = 0; i <= 3; i++){
-            cout << result[i] << " " << endl; 
-        }
+int main() {
+  string str = "aaBBcc123";
+  regex pat("([a-zA-Z]{6})(\\d{3})");
+  smatch result;
+  bool a = regex_match(str, result, pat);
+  if (a) {
+    cout << "ture：" << result[1] << endl;
+    cout << result.size();
+    for (int i = 0; i <= 3; i++) {
+      cout << result[i] << " " << endl;
     }
-    else{
-        cout << "fail" << endl; 
-    }
-    return 0;
+  } else {
+    cout << "fail" << endl;
+  }
+
+  return 0;
 }
