53ebfbeec2554f77998e901c056cc47b1c04cbc9
Doeach Du <55071540+doeach-dw@users.noreply.github.com>
2019-12-27 11:01:38 +0800

    Regular expression (#4)
    Resolve #1
    
    Signed-off-by: doeach-dw <doeach@qq.com>

---
+++ Makefile
@@ -0,0 +1,16 @@
+CFLAGS = -Wall -Werror 
+
+all: Re
+
+Re: Re_expression.cc
+	g++ $^ -o $@ $(CFLAGS)
+
+check: 
+	@echo Done
+
+distcheck:
+	@echo
+
+clean:
+	@rm -vf  Re
+
---
+++ Re_expression.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <regex>
+using namespace std;
+
+int main() {
+  string str = "aaBBcc123";
+  regex pat("([a-zA-Z]{6})(\\d{3})");
+  smatch result;
+  bool a = regex_match(str, result, pat);
+  if (a) {
+    cout << "ture：" << result[1] << endl;
+    cout << result.size();
+    for (int i = 0; i <= 3; i++) {
+      cout << result[i] << " " << endl;
+    }
+  } else {
+    cout << "fail" << endl;
+  }
+
+  return 0;
+}
